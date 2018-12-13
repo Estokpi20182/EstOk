a5991f7f2cc697b94e7c5a002d8a0863d8e2d195
davidlaurent <davidlaurentdasilva@gmail.com>
2018-12-13 11:17:02 -0200

    Update estoque.py

--- estoque.py
+++ estoque.py
@@ -5,63 +5,63 @@ class estoque(BotPlugin):
     """Adicionar ao estoque"""
     @botcmd
     def adicionar(self, msg, args):
-        """Inicia"""
+        """Inicia e bot pergunta qual item listar."""
         yield "Qual item voce quer adiconar a lista?"
 
     @botmatch(r'^[A-z].*$', flow_only=True)
-    def aitem(self, msg, match):
-        """Usuario diz qual item adicionar"""
-        aitem = match.string
-        aessecara = pymongo.MongoClient().estoque.estoque 
-        bananao = aessecara.insert({"id": str(msg.frm), "item": aitem, "quantidade": 0})
+    def item_a_listar(self, msg, match):
+        """Usuario diz qual item adicionar a lista."""
+        item_a_listar = match.string
+        banco = pymongo.MongoClient().estoque.estoque 
+        adicionando_item = banco.insert({"id": str(msg.frm), "item": item_a_listar, "quantidade": 0})
 
     @botcmd
     def comprei(self, msg, args):
-        """Inicia"""
+        """Inicia e bot pergunta qual item e quantos adicionar em quantidade."""
         yield "O que e o quanto voce comprou?"
 
     @botmatch(r'^[A-z].* [0-9].*$', flow_only=True)
-    def item(self, msg, match):
+    def item_a_incrementar(self, msg, match):
         """Usuario diz oque, e o quanto comprou."""
-        item = match.string.split(' ')[0]
+        item_a_incrementar = match.string.split(' ')[0]
         quantidade = match.string.split(' ')[1]
-        yield "Adicionando " + quantidade + " " + item + "(s). "
-        essecara = pymongo.MongoClient().estoque.estoque
-        resultado = essecara.find_one_and_update({ "id": str(msg.frm), "item" : item}, {'$inc': { "quantidade": int(quantidade)}})
-        banana = pymongo.MongoClient().estoque.estoque
-        bananao = banana.find_one({"id": str(msg.frm), "item": item})
-        yield "Show, agora voce tem " + str(int(bananao["quantidade"])) + " " + bananao["item"] + "(s)."
+        yield "Adicionando " + quantidade + " " + item_a_incrementar + "(s). "
+        banco_update = pymongo.MongoClient().estoque.estoque
+        incrementando = banco_update.find_one_and_update({ "id": str(msg.frm), "item" : item_a_incrementar}, {'$inc': { "quantidade": int(quantidade)}})
+        banco_find = pymongo.MongoClient().estoque.estoque
+        verificacao_do_estoque = banco_find.find_one({"id": str(msg.frm), "item": item_a_incrementar})
+        yield "Show, agora voce tem " + str(int(verificacao_do_estoque["quantidade"])) + " " + verificacao_do_estoque["item"] + "(s)."
 
 
     @botcmd
     def usei(self, msg, args):
-        """Inicia"""
+        """Inicia e bot pergunta qual item e quantos remover em quantidade."""
         yield "O que e o quanto voce usou?"
 
     @botmatch(r'^[A-z].* [0-9].*$', flow_only=True)
-    def uitem(self, msg, match):
+    def item_a_reduzir(self, msg, match):
         """Usuario diz oque, e o quanto usou."""
-        uitem = match.string.split(' ')[0]
-        uquantidade = match.string.split(' ')[1]
-        yield "Removendo " + uquantidade + " " + uitem + "(s). "
-        uessecara = pymongo.MongoClient().estoque.estoque
-        uresultado = uessecara.find_one_and_update({ "id": str(msg.frm), "item" : uitem}, {'$inc': { "quantidade": -(int(uquantidade))}})
-        ubanana = pymongo.MongoClient().estoque.estoque
-        ubananao = ubanana.find_one({"id": str(msg.frm), "item": uitem})
-        yield "Show, agora voce tem " + str(int(ubananao["quantidade"])) + " " + ubananao["item"] + "(s)."
+        item_a_reduzir = match.string.split(' ')[0]
+        quantidade = match.string.split(' ')[1]
+        yield "Removendo " + quantidade + " " + item_a_reduzir + "(s). "
+        banco_update = pymongo.MongoClient().estoque.estoque
+        reduzindo = banco_update.find_one_and_update({ "id": str(msg.frm), "item" : item_a_reduzir}, {'$inc': { "quantidade": -(int(quantidade))}})
+        banco_find = pymongo.MongoClient().estoque.estoque
+        verificacao_do_estoque = banco_find.find_one({"id": str(msg.frm), "item": item_a_reduzir})
+        yield "Top, agora voce tem " + str(int(verificacao_do_estoque["quantidade"])) + " " + verificacao_do_estoque["item"] + "(s)."
 
     @botcmd
     def estoque(self, msg, args):
-        """Cliente perguntando quanto tem de algo"""
+        """Inicia e bot pergunta qual item usuario quer verificar."""
         yield "Qual item?"
 
     @botmatch(r'^[A-z].*$', flow_only=True)
-    def qitem(self, msg, match):
-        """Perguntar de qual item"""
-        item = match.string
-        cliente = pymongo.MongoClient().estoque.estoque
-        quantidade = cliente.find_one({"id": str(msg.frm), "item": item})
-        yield "Voce tem " + str(int(quantidade["quantidade"])) + " " + quantidade["item"] + "(s)."
+    def item_a_verificar(self, msg, match):
+        """Usuario diz qual item quer verificar"""
+        item_a_verificar = match.string
+        banco_find = pymongo.MongoClient().estoque.estoque
+        estoque = banco_find.find_one({"id": str(msg.frm), "item": item_a_verificar})
+        yield "Voce tem " + str(int(estoque["quantidade"])) + " " + estoque["item"] + "(s)."
 
     @botcmd
     def saymyname(self, msg, args):
